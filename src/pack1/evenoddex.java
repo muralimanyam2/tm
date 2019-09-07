0d33bb1978f08ed9330bc2beedbefe220281866f
Prathap <prathap.ufttest@gmail.com>
2019-09-07 10:42:18 +0530

    commit

---
+++ src/pack1/evenoddex.java
@@ -0,0 +1,22 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class evenoddex {
+
+	public static void main(String[] args) {
+		
+Scanner sc =new Scanner(System.in);
+System.out.println("enter value");
+int n=sc.nextInt();
+Scanner g =new Scanner(System.in);
+System.out.println("enter value");
+int k=g.nextInt();
+
+for(int i=1;i<=k;i++)
+{
+         	
+System.out.println(n+" x" +i+ "=  " + (n*i));
+	}
+	}
+}
